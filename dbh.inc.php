17f755ec6e7a2c736aeb4e84adad780035e3f2ac
adham1988 <54411056+adham1988@users.noreply.github.com>
2019-09-02 10:30:57 +0200

    Delete dbh.inc.php

--- dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "password";
-$dbName = "loginSystem";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
